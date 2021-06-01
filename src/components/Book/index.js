a609e072fda3ec657e0b04bf828e4f89557d8bad
Mohammad Moien <mnmoienian@gmail.com>
2021-06-01 11:02:24 +0430

    fix some styles problems in cards

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -11,8 +11,7 @@ const Book = ({ book }) => {
         />
       </div>
       <div className="card__title">{book.volumeInfo.title}</div>
-      <div className="card_subtitle">{book.volumeInfo.subTitle}</div>
-      <div>{book.volumeInfo.authors}</div>
+      <div className="card__authors">{book.volumeInfo.authors}</div>
       <div className="card_description">{book.volumeInfo.description}</div>
     </div>
   );
